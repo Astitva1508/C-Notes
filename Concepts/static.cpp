224e2d160c4e59a898d5338a13645748b6fb4a1a
Astitva1508 <21bcs015@nith.ac.in>
2023-02-09 10:58:03 +0530

    Commiting Intermediary Changes

---
+++ Concepts/static.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
+
+void theory(){
+    // Static Variables have a property of preserving their values even after they are out of their scope. Once initialized , they can be used throughout the program. 
+    // Allocated memory in the data segment
+}
